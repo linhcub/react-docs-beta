4b220c95f9a2021835fbf834fecdcceee4d2db0b
prd-linh-le <linh.le@paradox.ai>
2022-11-30 23:27:50 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './components/Profile';
+import ShoppingList from './components/ShoppingList';
+import { useState } from 'react'
+
+let isLoggedIn = true
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoggedIn && <Profile />}
+      <ShoppingList />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </div>
+  );
+}
+
+function MyButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
+      Click {count} times
+    </button>
   );
 }
 
