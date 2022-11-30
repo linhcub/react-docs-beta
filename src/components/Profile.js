0bcca7e3faf529be3efd6dde6ea740e7fdbe24e1
prd-linh-le <linh.le@paradox.ai>
2022-12-01 00:15:53 +0700

    messi

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,9 @@
 import './Profile.css'
 
 const user = {
-    name: 'Hedy Lamarr',
-    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-    imageSize: 90,
+    name: 'Lionel Messi',
+    imageUrl: 'https://i.guim.co.uk/img/media/d5005bc5b7bed47c8ec60d068ac87e39f8bad313/0_0_2560_1536/master/2560.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=eb1d21ed68cb8ae3f009229625b8845e',
+    imageSize: 200,
 };
 
 export default function Profile() {
